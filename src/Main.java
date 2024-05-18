602b0e4fa5fdfdbe887b750152b46e2744f0012d
kamalshabanow <kamalshabanov04@gmail.com>
2024-05-18 13:00:37 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class MyFirstApp {
+//    public static void main(String[] args) {
+//        User u = new User();
+//
+//        String deyisen = null ;
+//        System.out.println(deyisen);
+//    }
+
+}
